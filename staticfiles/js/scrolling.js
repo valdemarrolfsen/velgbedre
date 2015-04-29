cee9c8193d0b3f988a2305106ff9c1d129e10fff
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2015-04-29 21:41:10 +0200

    Endringern på layout

--- staticfiles/js/scrolling.js
+++ staticfiles/js/scrolling.js
@@ -4,7 +4,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 70
+            scrollTop: $($anchor.attr('href')).offset().top - 60
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
