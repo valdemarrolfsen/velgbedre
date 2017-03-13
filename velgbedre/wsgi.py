c270c939150c44080344cdc910bcdb27e3544afe
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2017-03-13 16:23:14 +0100

    Fixin wsgi file

--- velgbedre/wsgi.py
+++ velgbedre/wsgi.py
@@ -8,14 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "velgbedre.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-root_path = os.path.abspath(os.path.split(__file__)[0])
-sys.path.insert(0, os.path.join(root_path, 'project_name'))
-sys.path.insert(0, root_path)
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
