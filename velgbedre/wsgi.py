7e8c0d51b71731ae9e4831f09195206396a6cbc5
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2017-03-13 15:59:52 +0100

    Alteres wsgi file

--- velgbedre/wsgi.py
+++ velgbedre/wsgi.py
@@ -6,10 +6,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "velgbedre.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
